7be47edc45e1f1e20464f0c5b9865b1a56fa830c
yvnat <44072408+yvnat@users.noreply.github.com>
2018-10-11 18:16:24 -0400

    made this actually function

--- source/terminalapi/winapi.cpp
+++ source/terminalapi/winapi.cpp
@@ -1,10 +1,14 @@
 #include "windows.h"
+#include <string>	//this is good because it prevents the error
+#include <iostream>
+
+using namespace std;
 
 class CRI {	//console renovation initiative (pronounced "cry")
 	public:
 	HANDLE STDHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 	void setConsoleTitle(string title) {
-		SetConsoleTitle((LPCSTR)title);
+		SetConsoleTitle(title.c_str());
 	}
 	void setCursorVisible(bool showFlag) {\
 		CONSOLE_CURSOR_INFO cursorInfo;
